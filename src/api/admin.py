c5bf7742fbeca9bfec83df16a089e418aa878db1
salemmohdmohd <salim.farouq@gmail.com>
2025-08-28 16:23:42 -0500

    Feat/backend - Database models and SQLAlchemy setup

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,17 +1,32 @@
-  
+
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import (db, User, Organization, Category, Location, UserBookmark, SearchHistory,
+                    OrganizationPhoto, OrganizationSocialLink, ContactMessage,
+                    Notification, AuditLog, Advertisement)
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-    admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
+    admin = Admin(app, name='Charity Directory Admin', template_mode='bootstrap3')
 
-    
-    # Add your models here, for example this is how we add a the User model to the admin
+    # Core models
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Organization, db.session))
+    admin.add_view(ModelView(Category, db.session))
+    admin.add_view(ModelView(Location, db.session))
 
-    # You can duplicate that line to add mew models
-    # admin.add_view(ModelView(YourModelName, db.session))
+    # Relationship models
+    admin.add_view(ModelView(UserBookmark, db.session))
+    admin.add_view(ModelView(SearchHistory, db.session))
+
+    # Organization related models
+    admin.add_view(ModelView(OrganizationPhoto, db.session))
+    admin.add_view(ModelView(OrganizationSocialLink, db.session))
+    admin.add_view(ModelView(ContactMessage, db.session))
+
+    # System models
+    admin.add_view(ModelView(Notification, db.session))
+    admin.add_view(ModelView(AuditLog, db.session))
+    admin.add_view(ModelView(Advertisement, db.session))
