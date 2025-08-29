328458f6dcc4a24814a65efbf39f8e1cd6f6c906
Miryre <e.d.reynolds@icloud.com>
2025-08-29 16:42:36 +0000

    basic footer

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,11 +1,14 @@
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
+	<footer 
+		className="footer mt-auto py-3 text-center"
+		style={{ backgroundColor: '#343a40', color: '#fff' }}
+	>
 		<p>
-			Check the <a target="_blank" href="https://4geeks.com/docs/start/react-flask-template">template documentation</a> <i className="fa-solid fa-file"></i> for help.
+			Connecting communities with charities that make a difference.
 		</p>
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			Made with <i className="fa fa-heart" style={{ color: '#28a745' }} /> by{" "}
+			<span style={{ color: '#28a745', fontWeight: '600' }}>Unseen Team</span>
 		</p>
 	</footer>
 );
