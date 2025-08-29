4b2d5147e8edfc9efef7c613dca89ddd0020cdc7
salemmohdmohd <salim.farouq@gmail.com>
2025-08-29 11:07:46 -0500

    Seed data

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,9 +1,7 @@
 
 import os
 from flask_admin import Admin
-from .models import (db, User, Organization, Category, Location, UserBookmark, SearchHistory,
-                    OrganizationPhoto, OrganizationSocialLink, ContactMessage,
-                    Notification, AuditLog, Advertisement)
+from .models import (db, User, Organization, Category, Location, UserBookmark, SearchHistory, OrganizationPhoto, OrganizationSocialLink, ContactMessage, Notification, AuditLog, Advertisement)
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
