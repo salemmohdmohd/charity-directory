a4ca13507caf2ae84ae5ba189d4ad4f61faa14dc
Miryre <e.d.reynolds@icloud.com>
2025-08-29 15:15:47 +0000

    created navbar links

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import React, { useState} from 'react';
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
@@ -6,11 +7,23 @@ export const Navbar = () => {
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1">Unseen</span>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+					<Link to="/categories">
+						Categories
+					</Link>
+					<br/>
+					<Link to="/login/signup">
+						Login/Signup
+					</Link>
+					<br/>
+					<Link to="/about us">
+						About Us
+					</Link>
+					<br/>
+					<Link to="/list your charity">
+						List Your Charity +
 					</Link>
 				</div>
 			</div>
