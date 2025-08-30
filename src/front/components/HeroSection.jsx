70dd337436b2d93aab97a051d92c8ca1a2a63655
Miryre <e.d.reynolds@icloud.com>
2025-08-30 15:59:06 +0000

    added more for the hero section

--- src/front/components/HeroSection.jsx
+++ src/front/components/HeroSection.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <header> className='bg-primary text-white text-center py-5'</header>
-    <div>
-      <p>Hello I'm here</p>
-    </div>
-  )
-}
+    <header className='bg-primary text-white text-center py-5'>
+        <div className='container'>
+            <h1 className='display-4 mb-3'>Without You There Is No Us</h1>
+            <p className='lead mb-4'>
+                Support For Your Causes
+            </p>
+            <button className='btn btn-light btn-lg'>Learn More</button>
+        </div>
+    </header>
+  );
+};
 
 export default HeroSection
