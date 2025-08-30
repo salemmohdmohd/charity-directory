ce3149f0a1ec2ba54c132a0d9e6bbb54b874c28f
Miryre <e.d.reynolds@icloud.com>
2025-08-30 15:50:05 +0000

    started hero scetion

---
+++ src/front/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <header> className='bg-primary text-white text-center py-5'</header>
+    <div>
+      <p>Hello I'm here</p>
+    </div>
+  )
+}
+
+export default HeroSection
